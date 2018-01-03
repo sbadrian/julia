1ddc8b8ac1fd419bc3e846e287e32c20b05d4e3f
Chris Foster <chris42f@gmail.com>
2018-01-03 22:54:21 +1000

    Improved ConsoleLogger key value pair formatting
    * Use show() and showerror() for formatting values in ConsoleLogger key
      value pairs.
    * Set the :limit IOContext key to sensibly show approximations of large
      data structures
    * Add an option to control whether the log level prefixes the message.

--- stdlib/Logging/src/ConsoleLogger.jl
+++ stdlib/Logging/src/ConsoleLogger.jl
@@ -1,22 +1,44 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
 """
-    ConsoleLogger(stream=STDERR, min_level=Info)
+    ConsoleLogger(stream=STDERR, min_level=Info; min_level_prefix=Logging.Warn, show_limited=true)
 
 Logger with formatting optimized for readability in a text console, for example
 interactive work with the Julia REPL.
 
 Log levels less than `min_level` are filtered out.
+
+Message formatting can be controlled by setting keyword arguments.
+`min_level_prefix` controls whether the name of the log level will prefix the
+message (for example `@warn "blah"` being formatted as "Warning: blah").  The
+printing of large data structures in key value pairs is limited to what can be
+reasonably shown on the display when `show_limited=true`, by setting the
+`:limit` `IOContext` key during formatting.
 """
 struct ConsoleLogger <: AbstractLogger
     stream::IO
     min_level::LogLevel
+    min_level_prefix::LogLevel
+    show_limited::Bool
     message_limits::Dict{Any,Int}
 end
-ConsoleLogger(stream::IO=STDERR, min_level=Info) = ConsoleLogger(stream, min_level, Dict{Any,Int}())
+function ConsoleLogger(stream::IO=STDERR, min_level=Info;
+                       min_level_prefix=Logging.Warn, show_limited=true)
+    ConsoleLogger(stream, min_level, min_level_prefix, show_limited, Dict{Any,Int}())
+end
 
 shouldlog(logger::ConsoleLogger, level, _module, group, id) =
     get(logger.message_limits, id, 1) > 0
 
 min_enabled_level(logger::ConsoleLogger) = logger.min_level
+
+# Formatting of values in key value pairs
+showvalue(io, msg) = show(io, "text/plain", msg)
+function showvalue(io, e::Tuple{Exception,Any})
+    ex,bt = e
+    showerror(io, ex, bt; backtrace = bt!=nothing)
+end
+showvalue(io, ex::Exception) = showvalue(io, (ex,catch_backtrace()))
 
 function handle_message(logger::ConsoleLogger, level, message, _module, group, id,
                         filepath, line; maxlog=nothing, kwargs...)
@@ -33,23 +55,27 @@ function handle_message(logger::ConsoleLogger, level, message, _module, group, i
                             Base.error_color()
     buf = IOBuffer()
     iob = IOContext(buf, logger.stream)
+    if logger.show_limited
+        iob = IOContext(iob, :limit=>true)
+    end
     msglines = split(chomp(string(message)), '\n')
-    prefixlevel = level >= Warn
-    width = displaysize(logger.stream)[2]
+    prefixwithlevel = level >= logger.min_level_prefix
+    dsize = displaysize(logger.stream)
+    width = dsize[2]
     locationstr = string(levelstr, " @ ", _module, " ", basename(filepath), ":", line)
     singlelinewidth = 2 + length(msglines[1]) +
-                      (prefixlevel ? length(levelstr) + 2 : 0) +
+                      (prefixwithlevel ? length(levelstr) + 2 : 0) +
                       length(locationstr)
     if length(msglines) + length(kwargs) == 1 && singlelinewidth <= width
         print_with_color(color, iob, "[ ", bold=true)
-        if prefixlevel
+        if prefixwithlevel
             print_with_color(color, iob, levelstr, ": ", bold=true)
         end
         print(iob, msglines[1])
         print(iob, " "^(width - singlelinewidth))
     else
         print_with_color(color, iob, "┌ ", bold=true)
-        if prefixlevel
+        if prefixwithlevel
             print_with_color(color, iob, levelstr, ": ", bold=true)
         end
         println(iob, msglines[1])
@@ -57,9 +83,23 @@ function handle_message(logger::ConsoleLogger, level, message, _module, group, i
             print_with_color(color, iob, "│ ", bold=true)
             println(iob, msglines[i])
         end
+        valbuf = IOBuffer()
+        valio = IOContext(IOContext(valbuf, iob),
+                          :displaysize=>(1 + dsize[1]÷(length(kwargs)+1),dsize[2]-5))
         for (key,val) in pairs(kwargs)
             print_with_color(color, iob, "│ ", bold=true)
-            println(iob, "  ", key, " = ", val)
+            print(iob, "  ", key, " =")
+            showvalue(valio, val)
+            vallines = split(String(take!(valbuf)), '\n')
+            if length(vallines) == 1
+                println(iob, " ", vallines[1])
+            else
+                println(iob)
+                for line in vallines
+                    print_with_color(color, iob, "│    ", bold=true)
+                    println(iob, line)
+                end
+            end
         end
         print_with_color(color, iob, "└ ", bold=true)
         print(iob, " "^(max(1, width - 1 - 1 - length(locationstr))))
--- stdlib/Logging/test/runtests.jl
+++ stdlib/Logging/test/runtests.jl
@@ -2,6 +2,8 @@ using Logging
 
 import Logging: min_enabled_level, shouldlog, handle_message
 
+@noinline func1() = backtrace()
+
 @testset "Logging" begin
 
 @testset "ConsoleLogger" begin
@@ -18,12 +20,14 @@ import Logging: min_enabled_level, shouldlog, handle_message
     @test shouldlog(logger, Logging.Info, Base, :group, :asdf) === false
 
     # Log formatting
-    function genmsg(level, message, _module, filepath, line; color=false, kws...)
+    function genmsg(level, message, _module, filepath, line; color=false, min_level_prefix=Logging.Warn, show_limited=true, kws...)
         buf = IOBuffer()
         io = IOContext(buf, :displaysize=>(30,75), :color=>color)
-        logger = ConsoleLogger(io, Logging.Debug)
-        handle_message(logger, level, message, _module, :group, :id,
-                       filepath, line; kws...)
+        logger = ConsoleLogger(io, Logging.Debug,
+                               min_level_prefix=min_level_prefix,
+                               show_limited=show_limited)
+        Logging.handle_message(logger, level, message, _module, :group, :id,
+                               filepath, line; kws...)
         s = String(take!(buf))
     end
 
@@ -43,6 +47,17 @@ import Logging: min_enabled_level, shouldlog, handle_message
     """
     [ Error: msg                                       Error @ Main path.jl:101
     """
+
+    # Disabling / enabling the level prefix
+    @test genmsg(Logging.Debug, "msg", Main, "some/path.jl", 101, min_level_prefix=Logging.BelowMinLevel) ==
+    """
+    [ Debug: msg                                       Debug @ Main path.jl:101
+    """
+    @test genmsg(Logging.Error, "msg", Main, "some/path.jl", 101, min_level_prefix=Logging.AboveMaxLevel) ==
+    """
+    [ msg                                              Error @ Main path.jl:101
+    """
+
     # Long line
     @test genmsg(Logging.Error, "msg msg msg msg msg msg msg msg msg msg msg msg msg", Main, "some/path.jl", 101) ==
     """
@@ -63,9 +78,73 @@ import Logging: min_enabled_level, shouldlog, handle_message
     """
     ┌ Error: msg
     │   a = 1
-    │   b = asdf
+    │   b = "asdf"
     └                                                 Error @ Base other.jl:101
     """
+    # Exceptions use showerror
+    @test genmsg(Logging.Info, "msg", Base, "other.jl", 101, exception=DivideError()) ==
+    """
+    ┌ msg
+    │   exception = DivideError: integer division error
+    └                                                  Info @ Base other.jl:101
+    """
+    # Attaching backtraces
+    bt = func1()
+    @test startswith(genmsg(Logging.Info, "msg", Base, "other.jl", 101,
+                            exception=(DivideError(),bt)),
+    """
+    ┌ msg
+    │   exception =
+    │    DivideError: integer division error
+    │    Stacktrace:
+    │     [1] func1() at""")
+
+
+    # Keywords - limiting the amount which is printed
+    @test genmsg(Logging.Info, "msg", Main, "some/path.jl", 101,
+                 a=fill(1.00001, 100,100), b=fill(2.00002, 10,10)) ==
+    """
+    ┌ msg
+    │   a =
+    │    100×100 Array{Float64,2}:
+    │     1.00001  1.00001  1.00001  1.00001  …  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001     1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001     1.00001  1.00001  1.00001
+    │     ⋮                                   ⋱                           
+    │     1.00001  1.00001  1.00001  1.00001     1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001     1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001     1.00001  1.00001  1.00001
+    │   b =
+    │    10×10 Array{Float64,2}:
+    │     2.00002  2.00002  2.00002  2.00002  …  2.00002  2.00002  2.00002
+    │     2.00002  2.00002  2.00002  2.00002     2.00002  2.00002  2.00002
+    │     2.00002  2.00002  2.00002  2.00002     2.00002  2.00002  2.00002
+    │     ⋮                                   ⋱                           
+    │     2.00002  2.00002  2.00002  2.00002     2.00002  2.00002  2.00002
+    │     2.00002  2.00002  2.00002  2.00002     2.00002  2.00002  2.00002
+    │     2.00002  2.00002  2.00002  2.00002     2.00002  2.00002  2.00002
+    └                                                   Info @ Main path.jl:101
+    """
+    # Limiting the amount which is printed
+    @test genmsg(Logging.Info, "msg", Main, "some/path.jl", 101,
+                 a=fill(1.00001, 10,10), show_limited=false) ==
+    """
+    ┌ msg
+    │   a =
+    │    10×10 Array{Float64,2}:
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    │     1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001  1.00001
+    └                                                   Info @ Main path.jl:101
+    """
+
 
     # Basic color test
     @test genmsg(Logging.Info, "line1\nline2", Main, "some/path.jl", 101, color=true) ==
